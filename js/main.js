593a0d68b05c91591c0f23b3665f6dd1416cc1fd
Lucas <lucasnlescano@gmail.com>
2023-07-25 22:49:49 -0300

    Se arregló un error en el menú hamburguesa

--- js/main.js
+++ js/main.js
@@ -4,8 +4,10 @@ const cerrar = document.querySelector("#cerrar");
 
 abrir.addEventListener("click", () => {
     menu.classList.add("visible");
+    document.body.classList.add("abertura-menu");
 })
 
 cerrar.addEventListener("click", () => {
     menu.classList.remove("visible");
+    document.body.classList.remove("abertura-menu");
 })
